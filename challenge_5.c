7dc65769a9f22da1d0b155ac13231e099975ac93
Yaakoub tair <yaakoubt05@gmail.com>
2025-09-04 16:01:04 +0200

    challemge 5

---
+++ challenge_5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int n,som=0,i;
+    printf("saisir un nombre :");
+    scanf("%d",&n);
+    for ( i = 1; i <= n; i++)
+    {
+       som=som+i;
+    }
+    printf("printf la somme est %d:",som);
+    
+
+
+
+
+
+
+
+
+    return 0;
+}
