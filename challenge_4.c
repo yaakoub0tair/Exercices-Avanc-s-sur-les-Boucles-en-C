a85bf63cad8d8ce49d0f306e710c83c6a7bebeb4
Yaakoub tair <yaakoubt05@gmail.com>
2025-09-04 15:38:39 +0200

    challenge 4

--- challenge_4.c
+++ challenge_4.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+int main(){
+    int n,inverse,rev=0;
+    printf("donner mois un nombre de 3 ou plus chiffre:\n");
+    scanf("%d",&n);
+
+    while (n>0)
+    {
+        inverse = n % 10;
+        rev = rev * 10 + inverse;
+        n = n / 10;
+         /* code */
+    }
+    printf("i'inverse est : %d",rev);
+
+    
+
+
+
+
+
+
+
+
+
+    return 0;
+}
